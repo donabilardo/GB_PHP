df675775e6d046684fcc36b1c04661c711581524
unknown <don.abilardo@gmail.com>
2024-02-20 21:28:10 +0500

    docker compose HW-2

--- code/index.php
+++ code/index.php
@@ -1,4 +1,28 @@
-<?php
-echo "Hello from container (sem 1)";
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Основы PHP</title>
+    <style>
+        .container {
+            max-width: 1200px;
+            margin: 0 auto;
+            padding: 0px 20px;
+        }
+    </style>
+</head>
+<body>
 
-var_dump(phpinfo());
+<div class="container">
+    <h1>Основы PHP</h1>
+    <ul>
+        <li><a href="les1/hw/index.php">Задание 1</a></li>
+        <li><a href="les2/hw/index.php">Задание 2</a></li>
+    </ul>
+</div>
+
+</body>
+</html>
