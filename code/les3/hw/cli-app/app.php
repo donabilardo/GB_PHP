790e24db342f462d43fd2616cd105ac511eec1da
unknown <don.abilardo@gmail.com>
2024-02-24 23:14:25 +0500

    add autoload from composer

--- code/les3/hw/cli-app/app.php
+++ code/les3/hw/cli-app/app.php
@@ -1,8 +1,11 @@
 <?php
 
-require_once ('src/main.function.php');
-require_once ('src/template.function.php');
-require_once ('src/file.function.php');
+//require_once ('src/main.function.php');
+//require_once ('src/template.function.php');
+//require_once ('src/file.function.php');
+
+
+require_once('vendor/autoload.php');
 
 $result = main('config.ini');
 
